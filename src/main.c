181b1e97b2a4e8e5c50ef6353295c8d19a2d33e4
Fabio Amorelli <amorelli.ff@gmail.com>
2022-12-14 11:36:32 -0300

    add lab13

--- src/main.c
+++ src/main.c
@@ -4,30 +4,49 @@
 #include "red_pill.h"
 #include "stm32f10x.h"
 
-int main(void) {
-    RCC->APB2ENR = RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_AFIOEN;  // enable clock
-    AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_JTAGDISABLE;
+void delay(uint8_t mode) {
+    TIM1->CCER = 0x1U << 0;
+    TIM1->CCMR2 = 0x30;
 
-    GPIOA->CRH = 0x34433443;
-    GPIOA->CRL = 0x43344333;
+    switch (mode) {
+        case 1:  // 148Hz
+            TIM1->ARR = 34 - 1;
+            TIM1->PSC = 7200 - 1;
+            TIM1->CCR1 = 17 - 1;
+            break;
+        case 2:  // 132Hz
+            TIM1->ARR = 38 - 1;
+            TIM1->PSC = 7200 - 1;
+            TIM1->CCR1 = 19 - 1;
+            break;
+    }
+
+    TIM1->CNT = 0;
+    TIM1->SR = 0;
+    TIM1->CR1 = 1;
+
+    while ((TIM1->SR & 0x1U) == 0) {
+    }
+
+    TIM1->CR1 = 0;
+}
+
+void main() {
+    RCC->APB2ENR = RCC_APB2ENR_AFIOEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_TIM1EN;
+    AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_JTAGDISABLE;
 
     GPIOB->CRH = 0x44444444;
     GPIOB->CRL = 0x44444443;
 
-    lcd_init();
-    delay_ms(100);
-
-    lcd_print("Hello world!");
+    GPIOB->ODR = 0x0;
 
     while (1) {
-        if (!(GPIOB->IDR & RPB_SW1) || !(GPIOB->IDR & RPB_SW2)) {  // if sw1 pressed
-            GPIOA->ODR |= (RPA_LED1 | RPA_LED2 | RPA_LED3);        // turn on leds
-
-        } else {
-            GPIOA->ODR &= ~(RPA_LED1 | RPA_LED2 | RPA_LED3);  // turn off leds
+        if (!(GPIOB->IDR & RPB_SW1)) {
+            GPIOB->ODR ^= RPB_BUZZ;
+            delay(2);
+        } else if (!(GPIOB->IDR & RPB_SW2)) {
+            GPIOB->ODR ^= RPB_BUZZ;
+            delay(1);
         }
     }
-
-    /* Will never reach this return */
-    return 0;
 }
